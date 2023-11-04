3f9b239abbf4a2e63a64d788d38ad31dd921d862
theneoalpha <vikashkaushik910@gmail.com>
2023-11-04 16:39:48 +0530

    Added buy ticket and transfer ticket function

--- Event_Organization.sol
+++ Event_Organization.sol
@@ -29,5 +29,22 @@ contract EventContract {
  }
 
 
+ function buyTicket(uint id,uint quantity) external payable{
+   require(events[id].date!=0,"Event does not exist");
+   require(events[id].date>block.timestamp,"Event has already occured");
+   Event storage _event = events[id];
+   require(msg.value==(_event.price*quantity),"Ethere is not enough");
+   require(_event.ticketRemain>=quantity,"Not enough tickets");
+   _event.ticketRemain-=quantity;
+   tickets[msg.sender][id]+=quantity;
 
+
+ }
+  function transferTicket(uint id,uint quantity,address to) external{
+   require(events[id].date!=0,"Event does not exist");
+   require(events[id].date>block.timestamp,"Event has already occured");
+   require(tickets[msg.sender][id]>=quantity,"You do not have enough tickets");
+   tickets[msg.sender][id]-=quantity;
+   tickets[to][id]+=quantity;
+ }
 }
